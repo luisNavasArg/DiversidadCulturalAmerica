abbe1974d8912575ddd8e251b64244ac24b21447
LuisNavasArg <lpipnavas@gmail.com>
2019-09-27 01:52:55 -0300

    primera parte

---
+++ js/datos.js
@@ -0,0 +1,83 @@
+let abya_yala= [{concepto:"Es el nombre con que se conoce al continente que hoy se nombra América, que literalmente significaría tierra en plena madurez o tierra de sangre vital. Dicho nombre le fue dado por el pueblo Kuna en Panamá y en Colombia y la nación  Guna Yala  del actual Panamá, antes del  descubirmiento y arribo  de Cristóbal Colón y loseuropeos. El nombre es aceptado hoy ampliamente por los actuales indígenas como el nombre oficial del continente ancestral en oposición al nombre extranjero América. Otras naciones aborígenes le dieron otros distintos nombres al continente en susrespectivos idiomas de acuerdo con sus propias visiones culturales específicas del concepto de continente o de territorio ( Mayab  en el caso de los Mayas de la actual península de Yucatán), visiones que no se identificaban con la idea española del continente. Actualmente, en diferentes organizaciones, comunidades e instituciones indígenas y representantes de ellas de todo el continente prefieren su uso para referirse al territorio continental, en vez del término América. El uso de este nombre es asumido como una posición ideológica por quienes lo usan, argumentando que el nombre &quot;América&quot; o la expresión &quot;Nuevo Mundo&quot; serían propias de los colonizadores europeos y no de los pueblos originarios del continente.", 
+				 img:"imagenes/principal.png"},
+				 {concepto:`<p>Algunas consideraciones de la actualidad:
+En las últimas décadas América Latina ha experimentado, a diferentes velocidades y
+con rasgos variados en los distintos países, signifcativos progresos en su desarrollo
+económico y social, como lo demuestran el sostenido crecimiento económico, la
+reducción de la pobreza e importantes logros en diversos indicadores sociales. Sin
+embargo, la Comisión Económica para América Latina y el Caribe (CEPAL) ha enfatizado
+que estos avances están encontrando límites, ya sea para sostenerse o para
+expandirse, y que la región debe enfrentar los desafíos que representa el cierre de las
+brechas originadas por la heterogeneidad estructural, la vulnerabilidad externa y la
+persistencia de altos niveles de desigualdad. Los pueblos indígenas forman parte de los
+colectivos más desfavorecidos, como resultado de complejos procesos sociales e
+históricos que se iniciaron hace más de 500 años y que fueron estableciendo prácticas
+discriminatorias persistentes hasta el presente e implicaron un despojo sistemático de
+sus territorios, con graves consecuencias para su bienestar. No obstante, después del
+</p><p>
+fn de la Guerra Fría, en un mundo globalizado y pluricultural, la irrupción de los
+pueblos indígenas y su agenda de derechos se enmarca en una diversidad de luchas
+por el reconocimiento y la dignidad humana. Este fenómeno no es exclusivo de la
+región, sino un proceso global. La tenaz resistencia de los pueblos indígenas se expresa
+actualmente en la instauración de un nuevo estado de cosas en lo político y lo
+territorial, así como de nuevos modos de relación institucional entre los Estados y
+dichos pueblos. La Declaración de las Naciones Unidas sobre los Derechos de los
+Pueblos Indígenas ha sido el corolario de las prolongadas luchas indígenas y sintetiza el
+estándar internacional, obligatorio para los Estados, ofreciendo un marco normativo
+explícito como enfoque para las políticas públicas. Si bien en este libro se registran
+progresos indudables en materia de la aplicación de estos derechos, por otro lado se
+constata que las brechas persisten y que las desigualdades continúan siendo
+profundas. Uno de los mayores desafíos que enfrenta la región en su búsqueda de la
+igualdad es la inclusión de los derechos de los pueblos indígenas entre las prioridades
+de las políticas. Los retos son enormes, si se considera que en la región, como se
+examina en este estudio, existen más de 800 pueblos indígenas, con una población
+cercana a los 45 millones de personas, que se caracterizan por su amplia diversidad
+demográfca, social, territorial y política, e incluyen desde pueblos en aislamiento
+voluntario hasta pueblos que están presentes en grandes asentamientos urbanos. A
+esto se agrega que el crecimiento económico registrado en la región es altamente
+dependiente de los recursos naturales y de sus precios internacionales, al mismo
+tiempo que se observa una débil gobernanza de estos recursos. La reprimarización de
+la economía ha ocasionado fuertes presiones sobre los territorios de los pueblos
+indígenas y ha desencadenado numerosos confictos socioambientales todavía no
+resueltos. La propuesta actual de la CEPAL, plasmada en los documentos presentados
+en sus períodos de sesiones de 2010, 2012 y 20141 , es la de un desarrollo centrado en
+el valor de la igualdad con un enfoque de derechos. Asimismo, hemos señalado que es
+necesario rediseñar los equilibrios entre el Estado, el mercado y la sociedad para
+construir pactos en que concurran aquellos actores que garanticen acuerdos políticos
+de largo alcance. Con estas premisas, es indudable que los pueblos indígenas deben
+ser incluidos en tales pactos y que estos deben abrir espacios para avanzar más
+rápidamente en la aplicación de los estándares internacionales. Uno de los principales
+</p><p>
+desafíos en esta materia es la integración de los derechos de los pueblos indígenas en
+un nuevo modelo de gobernanza de los recursos naturales. Se trata de transitar desde
+los “nuevos tratos” hacia los “nuevos pactos”. Junto con lo anterior, los conocimientos
+ancestrales, las innovaciones y las prácticas tradicionales de los pueblos indígenas para
+la conservación y la utilización sostenible de la diversidad biológica, así como el
+desarrollo de las diferentes modalidades colectivas de las economías indígenas,
+ofrecen una oportunidad valiosa para la construcción de un nuevo paradigma de
+desarrollo, basado en un cambio estructural hacia la igualdad y la sostenibilidad. Es
+fundamental el reconocimiento del aporte de los pueblos indígenas en la superación
+de los retos que trae consigo el porvenir de nuestra región. En 2014, se llevó a cabo la
+reunión de alto nivel conocida como Conferencia Mundial sobre los Pueblos Indígenas,
+en la que los Estados Miembros de las Naciones Unidas aprobaron un documento fnal
+histórico, en el que renovaron su compromiso con la plena realización de los derechos
+de dichos pueblos. En 2015, la comunidad internacional aprobó la Agenda 2030 para el
+Desarrollo Sostenible y sus 17 Objetivos de Desarrollo Sostenible (ODS), con el
+compromiso de que nadie se quede atrás. Para cumplir con estos acuerdos, es preciso
+visibilizar nuestra genuina diversidad; de allí el título de este libro. En este contexto,
+resultan imprescindibles las voluntades políticas que permitan avanzar en el cabal
+cumplimiento de los derechos humanos de los pueblos indígenas, incluidos las
+mujeres, los niños y niñas, los jóvenes y las personas mayores, así como de su derecho
+a ser colectivos diferentes. Esto no solo constituye un asunto de justicia social, sino
+que contribuirá en gran medida a cimentar los cambios profundos que necesitan la
+región y el orbe, sentando bases más sólidas para la paz mundial.</p>`,img:""}];
+
+window.onload=()=>{
+	document.querySelector('#seccion').innerHTML=`
+	<article  class="col-8 text-justify">${abya_yala[0].concepto}</article>
+				<article class="col-4 ">
+					<img src="${abya_yala[0].img}" class="img-fluid">	
+				</article>
+	`;
+}
+
